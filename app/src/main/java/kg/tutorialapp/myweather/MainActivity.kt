7a8ea5ea5133f2f9142feebba2979ade819e6ee3
MyTwoFRagment <dzaparovap@gmail.com>
2021-07-05 23:01:41 +0600

    Clean code

--- app/src/main/java/kg/tutorialapp/myweather/MainActivity.kt
+++ app/src/main/java/kg/tutorialapp/myweather/MainActivity.kt
@@ -37,12 +37,12 @@ import kotlin.String as String
 class MainActivity : AppCompatActivity() {
 
 
-//    val example=SingleTonExample.getInstance()
+
     private var workResult=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        fetchWeatherUsingQuery()
+
         setup()
 //
     }
@@ -51,7 +51,7 @@ class MainActivity : AppCompatActivity() {
         val btn_start=findViewById<Button>(R.id.btn_start)
         val btn_show=findViewById<Button>(R.id.btn_showToast)
         btn_start.setOnClickListener {
-//            doSomeWork()
+//
             makeRxCall()
         }
         btn_show.setOnClickListener {
@@ -124,50 +124,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-//        val tv_counter=findViewById<TextView>(R.id.counter)
-//        for (i in 0..4) {
-//                Thread.sleep(1000)
-//                workResult++
-//            }
-//        tv_counter.text=workResult.toString()
-    //        Thread(Runnable {
-//            for (i in 0..4) {
-//                Thread.sleep(1000)
-//                workResult++
-//            }
-//            runOnUiThread{
-//                tv_counter.text=workResult.toString()
-//            }
-////            Handler(Looper.getMainLooper()).post(Runnable {
-////                tv_counter.text=workResult.toString()
-////            })
-//        }).start()
 
-
-//    private fun fetchWeatherUsingQuery() {
-//        val call = WeatherClient.weatherApi.fetchWeatherUsingQuery(lat = 40.513996, lon = 72.816101)
-//        val textView1 = findViewById<TextView>(R.id.textView1)
-//        val textView2 = findViewById<TextView>(R.id.textView2)
-//        call.enqueue(object : Callback<ForeCast> {
-//            override fun onResponse(call: Call<ForeCast>, response: Response<ForeCast>) {
-//                if (response.isSuccessful) {
-//                    val foreCast = response.body()
-//                    foreCast?.let {
-//                        textView1.text = it.current?.weather!![0].description
-//                        textView2.text = it.current?.temp.toString()
-////                        Toast.makeText(this@MainActivity, it.toString(), Toast.LENGTH_LONG).show()
-//                    }
-//
-//                }
-//
-//            }
-//
-//            override fun onFailure(call: Call<ForeCast>, t: Throwable) {
-//                Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_LONG).show()
-//            }
-//
-//        })
-//    }
     companion object{
         const val TAG="RX"
     }
